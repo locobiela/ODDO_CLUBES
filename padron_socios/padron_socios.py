0a3ef35da2a054ee93c99141b908945905f96c6e
Leandro Masutti <lmasutti@gmail.com>
2017-07-11 17:55:05 -0300

    Add files via upload

--- padron_socios/padron_socios.py
+++ padron_socios/padron_socios.py
@@ -24,14 +24,15 @@ from openerp.osv import fields, osv
 class res_partner(osv.osv):
     _inherit = 'res.partner'
     _columns = {
-    			"socio": fields.boolean("Socio"),
-    			"dni": fields.char("Documento")
-    			#"num": fields.integer("Numero de Socio"),
-    			#"call": fields.char("Señal Distintiva",size=6,help="Señal distintiva o numero de operador",translate=True),
-				#"cat": fields.selection([("0","Radioescucha"),("1","Inicial"),("2","Novicio"),("3","Intermedia"),("4","General"),("5","Superior"),("6","Especial"),],"Categoria"),
-				#"cond": fields.selection([("0","Cadete"),("1","Activo"),("2","Baja"),("3","Vitalicio"),("4","Honorario"),],"Condicion"),
-				#"pago": fields.date("Pago Hasta")
-    			}
-    _defaults = {
+       			"socio": fields.boolean("Socio"),
+    			"dni": fields.char("Documento"),
+    			"num": fields.integer("Numero de Socio"),
+    			"call": fields.char("Señal Distintiva",size=6,help="Señal distintiva o numero de operador",translate=True),
+				"cat": fields.selection([("0","Radioescucha"),("1","Inicial"),("2","Novicio"),("3","Intermedia"),("4","General"),("5","Superior"),("6","Especial"),],"Categoria"),
+				"cond": fields.selection([("0","Cadete"),("1","Activo"),("2","Baja"),("3","Vitalicio"),("4","Honorario"),],"Condicion"),
+				"pago": fields.many2one("account.period","code"),
+				"down_date": fields.datetime("Fecha de Baja"),
+				"motivo": fields.selection([("0","Voluntario"),("1","Exonerado"),("2","Fallecimiento"),("3","Mora"),],"Motivo"),
+				"acta": fields.char("Acta Numero"),
 				}
 res_partner()
